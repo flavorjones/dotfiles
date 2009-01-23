1a4d434a72c3ebac71ddb19d0b21b4a807bf59ec
mike <mike@e982e3f0-e8f2-0310-8746-bbe270d49345>
2009-01-23 04:28:32 +0000

    refactored files into subdirectories
    git-svn-id: svn+ssh://cyrano.daless.io/var/lib/svn/personal/trunk@748 e982e3f0-e8f2-0310-8746-bbe270d49345

--- setup.rb
+++ setup.rb
@@ -1,27 +1,35 @@
 #! /usr/bin/env ruby
 
+require 'find'
 require 'fileutils'
 
-# directory => [files]
-spec = {
-  'bin'         => %w[clup sshconf rsync_portable wvdialer m2h open_mailto g1-internets eclipse say webserver],
-  '.subversion' => %w[config],
-  '.vnc'        => %w[xstartup],
-  'Music'       => %w[normalize_volume.sh],
-  '.gdb'        => %w[session-hack session-ruby session-asm],
-  ENV['HOME']   => %w[.autotest .bashrc .exrc .gdbinit .gitconfig .gitignore .inputrc .mwmrc .sawfishrc .twmrc .Xdefaults .xmodmap4win],
-}
+# directory => [directory1, [source_directory2, dest_directory2], ...]
+specs = ['bin', '.subversion', '.vnc', 'Music', '.gdb', {'home' => ENV['HOME']}]
 
-spec.each do |directory, files|
-  fq_dest_dir = directory.match(%r(^/)) ? directory : File.join(ENV['HOME'], directory)
-  FileUtils.mkdir_p fq_dest_dir
+specs.each do |spec|
+  if spec.is_a?(Hash)
+    srcdir = spec.keys.first
+    destdir = spec[srcdir]
+  else
+    srcdir = spec
+    destdir = File.join(ENV['HOME'], srcdir)
+  end
+  FileUtils.mkdir_p destdir
+
+  files = []
+  Find.find(srcdir) do |file|
+    Find.prune if file =~ %r(/.svn$)
+    next if File.directory? file
+    files << file
+  end
+
   files.each do |file|
-    fq_src_file = File.join(File.dirname(__FILE__), file)
-    fq_dest_file = File.join(fq_dest_dir, file)
-    FileUtils.rm_f      fq_dest_file
-    FileUtils.ln        fq_src_file, fq_dest_file
+    src_file = File.join(File.dirname(__FILE__), file)
+    dest_file = File.join(destdir, File.basename(file))
+    puts "%30s => %s" % [src_file, dest_file]
+    FileUtils.rm_f dest_file
+    FileUtils.ln   src_file, dest_file
   end
 end
 
-# use directories and map directories instead of individual files.
 # make "old" directory, with xmodmap4win and others
