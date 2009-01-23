82e53f32ef652f75bd49e4b9896ca93e2e95a8be
mike <mike@e982e3f0-e8f2-0310-8746-bbe270d49345>
2009-01-23 03:52:40 +0000

    overhaul.
    git-svn-id: svn+ssh://cyrano.daless.io/var/lib/svn/personal/trunk@746 e982e3f0-e8f2-0310-8746-bbe270d49345

--- .gdbinit
+++ .gdbinit
@@ -1,7 +1,11 @@
 define session-ruby
-  source ~/.gdb/ruby
+  source ~/.gdb/session-ruby
 end
 
 define session-asm
-  source ~/.gdb/asm
+  source ~/.gdb/session-asm
+end
+
+define session-hack
+  source ~/.gdb/session-hack
 end
---
+++ setup.rb
@@ -0,0 +1,27 @@
+#! /usr/bin/env ruby
+
+require 'fileutils'
+
+# directory => [files]
+spec = {
+  'bin'         => %w[clup sshconf rsync_portable wvdialer m2h open_mailto g1-internets eclipse say webserver],
+  '.subversion' => %w[config],
+  '.vnc'        => %w[xstartup],
+  'Music'       => %w[normalize_volume.sh],
+  '.gdb'        => %w[session-hack session-ruby session-asm],
+  ENV['HOME']   => %w[.autotest .bashrc .exrc .gdbinit .gitconfig .gitignore .inputrc .mwmrc .sawfishrc .twmrc .Xdefaults .xmodmap4win],
+}
+
+spec.each do |directory, files|
+  fq_dest_dir = File.join(ENV['HOME'], directory)
+  FileUtils.mkdir_p fq_dest_dir
+  files.each do |file|
+    fq_src_file = File.join(File.dirname(__FILE__), file)
+    fq_dest_file = File.join(fq_dest_dir, file)
+    FileUtils.rm_f      fq_dest_file
+    FileUtils.ln        fq_src_file, fq_dest_file
+  end
+end
+
+# use directories and map directories instead of individual files.
+# make "old" directory, with xmodmap4win and others
