1512fc01673333ab5d8398ac913ffd2a3b5bf7b1
Mike Dalessio <mike@csa.net>
2011-11-30 12:17:57 -0500

    I <3 MENLO

--- setup.rb
+++ setup.rb
@@ -3,7 +3,7 @@
 require 'find'
 require 'fileutils'
 
-specs = ['bin', '.subversion', '.vnc', 'Music', '.gdb', '.ssh', {'home' => ENV['HOME']}]
+specs = ['bin', '.fonts', '.subversion', '.vnc', 'Music', '.gdb', '.ssh', {'home' => ENV['HOME']}]
 
 specs.each do |spec|
   if spec.is_a?(Hash)
