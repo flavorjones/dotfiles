7631611b4dfd11bfe3afb526ad0f52f2aecaac8f
Mike Dalessio <mike@csa.net>
2011-12-12 12:03:38 -0500

    OMG terrible keyconfig defaults removed

--- thunderbird/user.js
+++ thunderbird/user.js
@@ -1,14 +1,4 @@
 // from http://teleogistic.net/2011/03/making-the-thunderbird-interface-more-gmail-y/
 // stick me in your thunderbird profile directory for GMAIL-y keybindings.
-user_pref("keyconfig.main.key_killThread", "][I][");
-user_pref("keyconfig.main.key_markJunk", "meta shift][J][");
-user_pref("keyconfig.main.key_markReadByDate", "meta shift][D][");
-user_pref("keyconfig.main.key_markThreadAsRead", "meta shift][R][");
-user_pref("keyconfig.main.key_newMessage", "meta shift][M][");
-user_pref("keyconfig.main.key_newMessage2", "meta shift][N][");
 user_pref("keyconfig.main.key_nextMsg", "][J][");
 user_pref("keyconfig.main.key_previousMsg", "][K][");
-user_pref("keyconfig.main.key_reply", "][R][");
-user_pref("keyconfig.main.key_replyall", "shift][R][");
-user_pref("keyconfig.main.key_toggleMessagePane", "][V][");
-user_pref("keyconfig.main.xxx_key74_SwitchPaneFocus(event);", "][D][");
