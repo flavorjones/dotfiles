07fce82d48d2483890a5c0e6bdc25028cd9ecd4b
Mike Dalessio <mike.dalessio@gmail.com>
2017-04-28 15:23:02 -0400

    better devilspie2 script for pinning

--- devilspie2/always-on-workspace.lua
+++ devilspie2/always-on-workspace.lua
@@ -1,21 +1,8 @@
--- debug_print("Window Name: " .. get_window_name());
--- debug_print("Application name: " .. get_application_name());
+patterns = {"KeePassX", "emacs\d*"}
 
--- OMG I'm sorry about this.
-function list_iter (t)
-   local i = 0
-   local n = table.getn(t)
-   return function ()
-             i = i + 1
-             if i <= n then return t[i] end
-          end
-end
-
-patterns = {".* - KeePassX", "emacs\d*"}
-
-for pattern in list_iter(patterns) do
+for _, pattern in ipairs(patterns) do
    if (string.find(get_application_name(), pattern)) then
-      debug_print("Pinning '" .. get_application_name() .. "'")
+      debug_print(">> PINNING '" .. get_application_name() .. "'")
       pin_window()
    end
 end
