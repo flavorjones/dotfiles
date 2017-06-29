08cba3bae5aae984b5f59a9298c8f9c77d77ddda
Mike Dalessio <mike.dalessio@gmail.com>
2017-06-29 17:50:17 -0400

    add debug statement to devilspie2 config

--- devilspie2/always-on-workspace.lua
+++ devilspie2/always-on-workspace.lua
@@ -1,6 +1,7 @@
 patterns = {"KeePassX", "emacs\d*"}
 
 for _, pattern in ipairs(patterns) do
+   debug_print(">> EXAMINING '" .. get_application_name() .. "'")
    if (string.find(get_application_name(), pattern)) then
       debug_print(">> PINNING '" .. get_application_name() .. "'")
       pin_window()
