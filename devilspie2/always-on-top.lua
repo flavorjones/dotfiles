68ab9c280345000ccf1905cfb5a13d47b8a02753
Mike Dalessio <mike.dalessio@gmail.com>
2017-12-15 11:49:57 -0500

    google play music always on {top,desktop}

--- devilspie2/always-on-top.lua
+++ devilspie2/always-on-top.lua
@@ -1,6 +1,6 @@
-patterns = {"Zoom"}
+patterns = {"Zoom", "^Google Play Music$"}
 
--- debug_print("always-on-top      : examining '" .. get_application_name() .. "'")
+debug_print("always-on-top      : examining '" .. get_application_name() .. "'")
 for _, pattern in ipairs(patterns) do
    if (string.find(get_application_name(), pattern)) then
       debug_print("always-on-top      : TOPPING '" .. get_application_name() .. "'")
--- devilspie2/always-on-workspace.lua
+++ devilspie2/always-on-workspace.lua
@@ -1,6 +1,6 @@
-patterns = {"KeePassX", "emacs\d*", "Tomboy"}
+patterns = {"KeePassX", "emacs\d*", "Tomboy", "^Google Play Music$"}
 
--- debug_print("always-on-workspace: examining '" .. get_application_name() .. "'")
+debug_print("always-on-workspace: examining '" .. get_application_name() .. "'")
 for _, pattern in ipairs(patterns) do
    if (string.find(get_application_name(), pattern)) then
       debug_print("always-on-workspace: PINNING '" .. get_application_name() .. "'")
