2f5d2f88bbba05fd2c1420b2b4e5d2afb24b9374
Mike Dalessio <mike.dalessio@gmail.com>
2019-01-11 12:40:55 -0500

    devilspie: minimize windows on creation

---
+++ devilspie2/always-minimize-on-creation.lua
@@ -0,0 +1,11 @@
+patterns = {"^Zoom.*Pro Account$"}
+
+debug_print("always-minimize-on-creation: examining '" .. get_application_name() .. "'")
+for _, pattern in ipairs(patterns) do
+   if (string.find(get_application_name(), pattern)) then
+      debug_print("always-on-workspace: MINIMIZING '" .. get_application_name() .. "'")
+      minimize()
+      os.execute("sleep " .. 1) -- because sometimes apps explicitly maximize shortly after win creation
+      minimize()
+   end
+end
