2daa0f773337a5ed4f6975a72d78d91a75f805e4
Mike Dalessio <mike.dalessio@gmail.com>
2019-01-11 17:12:24 -0500

    devilspie: be more insistent on minimizing

--- devilspie2/always-minimize-on-creation.lua
+++ devilspie2/always-minimize-on-creation.lua
@@ -5,7 +5,10 @@ for _, pattern in ipairs(patterns) do
    if (string.find(get_application_name(), pattern)) then
       debug_print("always-on-workspace: MINIMIZING '" .. get_application_name() .. "'")
       minimize()
-      os.execute("sleep " .. 1) -- because sometimes apps explicitly maximize shortly after win creation
+      -- because sometimes apps (like Zoom) explicitly maximize shortly after win creation
+      os.execute("sleep " .. 1)
+      minimize()
+      os.execute("sleep " .. 1)
       minimize()
    end
 end
