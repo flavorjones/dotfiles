73f633086f3e83290a48f7807fe720513cc48fd6
Mike Dalessio <mike.dalessio@gmail.com>
2020-05-23 10:40:21 -0400

    devilspie: change title matching to work on free accounts

--- devilspie2/always-minimize-on-creation.lua
+++ devilspie2/always-minimize-on-creation.lua
@@ -1,4 +1,4 @@
-patterns = {"^Zoom.*Licensed Account$", "^Zoom Cloud Meetings$"}
+patterns = {"^Zoom.*Account$", "^Zoom Cloud Meetings$"}
 
 debug_print("always-minimize-on-creation: examining '" .. get_application_name() .. "'")
 for _, pattern in ipairs(patterns) do
