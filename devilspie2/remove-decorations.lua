3f292c53f7240ac3b4cc191385958307eb1380be
Mike Dalessio <mike.dalessio@gmail.com>
2020-11-03 11:13:18 -0500

    omg slack is the worst
    the notifications in the title bar are extremely distracting

---
+++ devilspie2/remove-decorations.lua
@@ -0,0 +1,13 @@
+patterns = {
+   "^Slack",
+   "^Terminal$",
+}
+
+
+debug_print("remove-decorations: examining '" .. get_application_name() .. "'")
+for _, pattern in ipairs(patterns) do
+   if (string.find(get_application_name(), pattern)) then
+      debug_print("remove-decorations: UNFRAMING '" .. get_application_name() .. "'")
+      undecorate_window()
+   end
+end
