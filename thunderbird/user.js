14ec761b4ee9f4e53657c61dbdd5276c92607022
Mike Dalessio <mike.dalessio@gmail.com>
2021-11-03 11:34:40 -0400

    removing old/unused files

--- thunderbird/user.js
+++
@@ -1,4 +0,0 @@
-// from http://teleogistic.net/2011/03/making-the-thunderbird-interface-more-gmail-y/
-// stick me in your thunderbird profile directory for GMAIL-y keybindings.
-user_pref("keyconfig.main.key_nextMsg", "][J][");
-user_pref("keyconfig.main.key_previousMsg", "][K][");
