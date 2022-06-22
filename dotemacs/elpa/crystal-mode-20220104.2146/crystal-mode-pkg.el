d6aa7006bc2ffba630e7e9cc74d41ead774fdaba
Mike Dalessio <mike.dalessio@gmail.com>
2022-06-22 15:20:50 -0400

    elpa update

---
+++ dotemacs/elpa/crystal-mode-20220104.2146/crystal-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "crystal-mode" "20220104.2146" "Major mode for editing Crystal files" '((emacs "24.4")) :commit "96a8058205b24b513d0b9307db32f05e30f9570b" :keywords '("languages" "crystal") :url "https://github.com/crystal-lang-tools/emacs-crystal-mode")
---
+++ dotemacs/elpa/elm-mode-20220227.931/elm-mode-pkg.el
@@ -0,0 +1,14 @@
+(define-package "elm-mode" "20220227.931" "Major mode for Elm"
+  '((f "0.17")
+    (s "1.7.0")
+    (emacs "25.1")
+    (seq "2.23")
+    (reformatter "0.3"))
+  :commit "1e277684d8a6681a2410cce2dd589ee30a998369" :authors
+  '(("Joseph Collard"))
+  :maintainer
+  '("Joseph Collard")
+  :url "https://github.com/jcollard/elm-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
