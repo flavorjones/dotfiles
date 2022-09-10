5b18b2b59dd210e137f71b1441f1839615368f30
Mike Dalessio <mike.dalessio@gmail.com>
2022-09-10 12:24:12 -0400

    i've been using cinnamon's "only use workspaces on primary monitor"
    so I don't need devilspie to micromanage this now

--- devilspie2/always-on-workspace.lua
+++ devilspie2/always-on-workspace.lua
@@ -11,10 +11,10 @@ patterns = {
    "^lofi.cafe.* - lofi music"
 }
 
-debug_print("always-on-workspace: examining '" .. get_application_name() .. "'")
-for _, pattern in ipairs(patterns) do
-   if (string.find(get_application_name(), pattern)) then
-      debug_print("always-on-workspace: PINNING '" .. get_application_name() .. "'")
-      pin_window()
-   end
-end
+-- debug_print("always-on-workspace: examining '" .. get_application_name() .. "'")
+-- for _, pattern in ipairs(patterns) do
+--    if (string.find(get_application_name(), pattern)) then
+--       debug_print("always-on-workspace: PINNING '" .. get_application_name() .. "'")
+--       pin_window()
+--    end
+-- end
