f87cf5585496315693238fcf53f58e17ad098756
Mike Dalessio <mike.dalessio@gmail.com>
2022-11-09 08:33:55 -0500

    update emacs packages

---
+++ dotemacs/elpa/crystal-mode-20221008.1847/crystal-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "crystal-mode" "20221008.1847" "Major mode for editing Crystal files" '((emacs "24.4")) :commit "9bfb9f0f566e937cc6a2f2913d1b56978b81dc99" :keywords '("languages" "crystal") :url "https://github.com/crystal-lang-tools/emacs-crystal-mode")
---
+++ dotemacs/elpa/rspec-mode-20221018.1123/rspec-mode-pkg.el
@@ -0,0 +1,13 @@
+(define-package "rspec-mode" "20221018.1123" "Enhance ruby-mode for RSpec"
+  '((ruby-mode "1.0")
+    (cl-lib "0.4"))
+  :commit "4437a243b2042585db994d77e6b7c85864a8d11a" :authors
+  '(("Peter Williams, et al."))
+  :maintainer
+  '("Peter Williams, et al.")
+  :keywords
+  '("rspec" "ruby")
+  :url "http://github.com/pezra/rspec-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
