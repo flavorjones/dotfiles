4644e5fceb79892ca7f5d97972262bde2ffa103a
Mike Dalessio <mike@37signals.com>
2024-09-07 14:50:01 -0400

    emacs: windsize
    should have been in the previous commit

---
+++ dotemacs/elpa/windsize-20181029.2257/windsize-pkg.el
@@ -0,0 +1,12 @@
+(define-package "windsize" "20181029.2257" "Simple, intuitive window resizing" 'nil :commit "62c2846bbe95b0a73e996c75e4a644d05f57aaaa" :authors
+  '(("Chris Perkins" . "chrisperkins99@gmail.com"))
+  :maintainers
+  '(("Chris Perkins" . "chrisperkins99@gmail.com"))
+  :maintainer
+  '("Chris Perkins" . "chrisperkins99@gmail.com")
+  :keywords
+  '("window" "resizing" "convenience")
+  :url "http://github.com/grammati/windsize")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
