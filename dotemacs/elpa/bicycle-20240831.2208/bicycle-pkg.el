7117d878c98ae9a21e888a85a0b5d4511b733382
Mike Dalessio <mike@37signals.com>
2024-09-25 17:41:10 -0400

    emacs: update packages

---
+++ dotemacs/elpa/bicycle-20240831.2208/bicycle-pkg.el
@@ -0,0 +1,15 @@
+(define-package "bicycle" "20240831.2208" "Cycle outline and code visibility"
+  '((emacs "26.1")
+    (compat "30.0.0.0"))
+  :commit "04c3e44eb10303b81c47c1d333df1fa23a224963" :authors
+  '(("Jonas Bernoulli" . "emacs.bicycle@jonas.bernoulli.dev"))
+  :maintainers
+  '(("Jonas Bernoulli" . "emacs.bicycle@jonas.bernoulli.dev"))
+  :maintainer
+  '("Jonas Bernoulli" . "emacs.bicycle@jonas.bernoulli.dev")
+  :keywords
+  '("outlines")
+  :url "https://github.com/tarsius/bicycle")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ dotemacs/elpa/monokai-theme-20240911.1046/monokai-theme-pkg.el
@@ -0,0 +1,10 @@
+(define-package "monokai-theme" "20240911.1046" "A fruity color theme for Emacs" 'nil :commit "dacd9d8a8867afea3ed76b15a6c997053ff88093" :authors
+  '(("Kelvin Smith" . "oneKelvinSmith@gmail.com"))
+  :maintainers
+  '(("Kelvin Smith" . "oneKelvinSmith@gmail.com"))
+  :maintainer
+  '("Kelvin Smith" . "oneKelvinSmith@gmail.com")
+  :url "http://github.com/oneKelvinSmith/monokai-emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
