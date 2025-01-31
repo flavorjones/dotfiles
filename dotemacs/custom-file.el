1e61d5de07c5edfdb689d70bdaaaf6997b6bc833
Mike Dalessio <mike@37signals.com>
2025-01-31 15:01:47 -0500

    emacs: fonts

--- dotemacs/custom-file.el
+++ dotemacs/custom-file.el
@@ -108,4 +108,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "JetBrains Mono" :foundry "JB  " :slant normal :weight medium :height 69 :width normal))))
  '(copilot-overlay-face ((t (:inherit (shadow highlight italic))))))
