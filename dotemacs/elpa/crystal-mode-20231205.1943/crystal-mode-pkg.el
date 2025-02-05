cf07c60c451dc2b5831d75379f068dde0d61c890
Mike Dalessio <mike@37signals.com>
2025-02-05 17:18:53 -0500

    emacs: update packages, drop auto-compile

--- dotemacs/elpa/crystal-mode-20231205.1943/crystal-mode-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "crystal-mode" "20231205.1943" "Major mode for editing Crystal files"
-  '((emacs "24.4"))
-  :commit "ea89b108fa4222df94ffb99e6e7eaec5d7aa4fea" :keywords
-  '("languages" "crystal")
-  :url "https://github.com/crystal-lang-tools/emacs-crystal-mode")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ dotemacs/elpa/crystal-mode-20250203.1157/crystal-mode-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "crystal-mode" "20250203.1157"
+  "Major mode for editing Crystal files."
+  '((emacs "24.4"))
+  :url "https://github.com/crystal-lang-tools/emacs-crystal-mode"
+  :commit "39993f821e6d7ca1da125d0ceba6218c3ca4c5b7"
+  :revdesc "39993f821e6d"
+  :keywords '("languages" "crystal"))
